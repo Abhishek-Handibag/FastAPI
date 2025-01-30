cb2e45d5bfa5821c012b62bf3e2492dbdd4638d0
Abhishek <abhishek.handibag12@gmail.com>
2025-01-30 07:54:12 +0530

    refresher for python

---
+++ basics.py
@@ -0,0 +1,9 @@
+#  How is Python interpreted?
+## Python code is executed line by line at runtime. Python internally converts the source code into an intermediate form called bytecode, which is then executed by the Python virtual machine (PVM).
+
+# How do you manage memory in Python?
+ ## Python uses automatic memory management and a garbage collector to handle memory. The garbage collector recycles memory when objects are no longer in use.
+
+
+# List: Mutable, can be changed after creation.
+# Tuple : Tuple: Immutable, cannot be changed after creation.
